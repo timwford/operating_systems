74b132774ef0c6f46a9b7d61f5e3cad092547c5f
Tim Ford <timothy.ford@mines.sdsmt.edu>
2021-04-11 15:36:16 -0600

    cool updates

--- dash/dash.cpp
+++ dash/dash.cpp
@@ -10,7 +10,9 @@ Contains main filesystem interaction
 #include <sys/resource.h>
 #include <sys/time.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>    /* For O_RDWR */
+#include <unistd.h>   /* For open(), creat() */
 #include <vector>
 #include <algorithm>
 
@@ -23,9 +25,13 @@ public:
   int status;
   int waitpid;
   int fd[2];
+  int saved_stdout;
   int who = RUSAGE_SELF;
   struct rusage usage;
   int ret;
+  string command;
+  vector<string> cmd1;
+  vector<string> cmd2;
 
   string COMMAND_NAME = "cmdnm";
   string PROCESS_ID = "pid";
@@ -170,9 +176,7 @@ public:
   }
 
   void handleCommand(vector<string> tokens) {
-    string command;
-    vector<string> cmd1;
-    vector<string> cmd2;
+    
 
     // find if there is a pipe or redirect
     bool isPipe = false;
@@ -182,22 +186,71 @@ public:
     if (std::find(tokens.begin(), tokens.end(), ">") != tokens.end()) isRedirect = true;
 
     if (isPipe || isRedirect) {
-      cout << "pipe or redirect" << endl;
-
       bool isCommand1 = true;
       for (auto &token: tokens) {
         if (token == "|" || token == ">") {
           isCommand1 = false;
         } else {
-          if (isCommand1) {
-            cmd1.push_back(token);
-          } else {
-            cmd2.push_back(token);
-          }
+          isCommand1 ? cmd1.push_back(token) : cmd2.push_back(token);
         }
       }
+
+      if (isPipe) {
+        handlePipedCommand();
+      } else if (isRedirect) {
+        handleRedirectCommand();
+      } else {
+        cout << "I messed up" << endl;
+      }
     
-      // create pipe
+    } else {
+      handleSingleCommand(tokens);
+    }
+
+    waitpid = wait(&status);
+    ret = getrusage(who, &usage);
+
+    cout << "\033[1;31mUser Time: \033[0m" << usage.ru_utime.tv_sec << "."
+         << usage.ru_utime.tv_usec << endl;
+    cout << "\033[1;31mSystem Time: \033[0m" << usage.ru_stime.tv_sec << "."
+         << usage.ru_stime.tv_usec << endl;
+    cout << "\033[1;31mPage faults: " << usage.ru_minflt << "\033[0m" << endl;
+    cout << "\033[1;31mSwaps: " << usage.ru_nswap << "\033[0m" << endl;
+  }
+
+  void handleRedirectCommand() {
+    int filefd = open(cmd2[0].c_str(), O_WRONLY|O_CREAT, 0666);
+    if (!fork()) {
+      close(1);//Close stdout
+      dup(filefd);
+
+      command = cmd1[0];
+      char **args = new char *[cmd1.size()];
+      for (size_t i = 0; i < cmd1.size(); i++) {
+        args[i] = new char[cmd1[i].size() + 1];
+        strcpy(args[i], cmd1[i].c_str());
+      }
+         
+      int error = execvp(command.c_str(), args);
+      if (error == -1) {
+        cout << endl << "Command not recognized" << endl << endl;
+        std::exit(-1);
+      }
+
+    } else {
+      close(filefd);
+      wait(NULL);
+    }
+
+    cout << cmd1[0] << endl;
+
+    cout << endl;
+
+    cout << cmd2[0] << endl;
+  }
+
+  void handlePipedCommand() {
+    // create pipe
       if (pipe(fd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
@@ -212,7 +265,7 @@ public:
           close(1);
           dup(fd[1]);
           close(fd[0]);
-          close(fd[0]);
+          close(fd[1]);
 
           command = cmd1[0];
           char **args = new char *[cmd1.size()];
@@ -220,13 +273,14 @@ public:
             args[i] = new char[cmd1[i].size() + 1];
             strcpy(args[i], cmd1[i].c_str());
           }
-
+          
           int error = execvp(command.c_str(), args);
           if (error == -1) {
             cout << endl << "Command not recognized" << endl << endl;
             std::exit(-1);
           }
 
+          wait(NULL);
           exit(1);
         } else if (pid2 < 0) {
           cout << "Fork unsuccessful" << endl;
@@ -234,6 +288,8 @@ public:
         } else {
           cout << "\033[1;32mNew child process ID: " << pid2 << "\033[0m" << endl;
         }
+
+        wait(NULL);
 
         // now the input side
         close(0);
@@ -253,9 +309,6 @@ public:
           cout << endl << "Command not recognized" << endl << endl;
           std::exit(-1);
         }
-
-        exit();
-
       } else if (pid1 > 0) {
         // Parent Process
         cout << "\033[1;32mNew child process ID: " << pid1 << "\033[0m" << endl;
@@ -263,8 +316,9 @@ public:
       } else {
         cout << "error: Fork was not successful" << endl;
       }
+  }
 
-    } else {
+  void handleSingleCommand(vector<string> tokens) {
       // single command
       char **args = new char *[tokens.size()];
       for (size_t i = 0; i < tokens.size(); i++) {
@@ -287,17 +341,5 @@ public:
       } else {
         cout << "error: Fork was not successful" << endl;
       }
-    }
-
-
-    waitpid = wait(&status);
-    ret = getrusage(who, &usage);
-
-    cout << "\033[1;31mUser Time: \033[0m" << usage.ru_utime.tv_sec << "."
-         << usage.ru_utime.tv_usec << endl;
-    cout << "\033[1;31mSystem Time: \033[0m" << usage.ru_stime.tv_sec << "."
-         << usage.ru_stime.tv_usec << endl;
-    cout << "\033[1;31mPage faults: " << usage.ru_minflt << "\033[0m" << endl;
-    cout << "\033[1;31mSwaps: " << usage.ru_nswap << "\033[0m" << endl;
   }
 };
